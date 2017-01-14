4c2d4c0e0af6f04b8f115db19ead9be80f761235
behring <zhaolin53636848@126.com>
2017-01-14 10:23:03 +0800

    delete platforms

--- platforms/android/assets/www/plugins/cordova-plugin-sharesdk/www/sharesdk.js
+++
@@ -1,7 +0,0 @@
-cordova.define("cordova-plugin-sharesdk.ShareSDK", function(require, exports, module) {
-/* global cordova */
-var share = module.exports = function (success, fail, shareInfo) {
-  cordova.exec(success, fail, 'ShareSDK', 'share', [shareInfo])
-}
-
-});
