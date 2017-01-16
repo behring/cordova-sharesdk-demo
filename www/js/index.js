01f77d59844285ad7ad7f229d64a9353f4c4d838
behring <zhaolin53636848@126.com>
2017-01-16 11:26:05 +0800

    添加分享文本到微信朋友圈或者微信好友

--- www/js/index.js
+++ www/js/index.js
@@ -33,19 +33,23 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
+        var textWechatSessionButton = parentElement.querySelector('.btn-text-wechat-session');
         var imageWechatSessionButton = parentElement.querySelector('.btn-image-wechat-session');
+        var textWechatTimelineButton = parentElement.querySelector('.btn-text-wechat-timeline');
         var imageWechatTimelineButton = parentElement.querySelector('.btn-image-wechat-timeline');
+        textWechatSessionButton.addEventListener('click',shareTextToWechatSession);
         imageWechatSessionButton.addEventListener('click',shareImageToWechatSession);
+        textWechatTimelineButton.addEventListener('click',shareTextToWechatTimeline);
         imageWechatTimelineButton.addEventListener('click',shareImageToWechatTimeline);
     }
 };
 
 app.initialize();
 
-function shareImageToWechatTimeline() {
-    var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
-    var shareInfo = {images:images};
-    sharesdk.share(ShareSDK.PlatformType.WechatTimeline,ShareSDK.ShareType.Image,shareInfo,
+function shareTextToWechatSession() {
+    var text='这是一条测试文本~~~~';
+    var shareInfo = {text:text};
+    sharesdk.share(ShareSDK.PlatformType.WechatSession,ShareSDK.ShareType.Text,shareInfo,
                    function(success){},
                    function(fail){});
 }
@@ -54,6 +58,22 @@ function shareImageToWechatSession() {
     var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
     var shareInfo = {images:images};
     sharesdk.share(ShareSDK.PlatformType.WechatSession,ShareSDK.ShareType.Image,shareInfo,
+                   function(success){},
+                   function(fail){});
+}
+
+function shareTextToWechatTimeline() {
+    var text='这是一条测试文本~~~~';
+    var shareInfo = {text:text};
+    sharesdk.share(ShareSDK.PlatformType.WechatTimeline,ShareSDK.ShareType.Text,shareInfo,
+                   function(success){},
+                   function(fail){});
+}
+
+function shareImageToWechatTimeline() {
+    var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
+    var shareInfo = {images:images};
+    sharesdk.share(ShareSDK.PlatformType.WechatTimeline,ShareSDK.ShareType.Image,shareInfo,
                    function(success){},
                    function(fail){});
 }
