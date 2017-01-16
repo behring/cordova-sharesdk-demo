880500d91e22083bded0af1dd1f689be65d83554
behring <zhaolin53636848@126.com>
2017-01-16 11:55:46 +0800

    添加分享链接到微信朋友圈或微信好友

--- www/js/index.js
+++ www/js/index.js
@@ -35,45 +35,72 @@ var app = {
         var parentElement = document.getElementById(id);
         var textWechatSessionButton = parentElement.querySelector('.btn-text-wechat-session');
         var imageWechatSessionButton = parentElement.querySelector('.btn-image-wechat-session');
+        var webPageWechatSessionButton = parentElement.querySelector('.btn-webpage-wechat-session');
+        
         var textWechatTimelineButton = parentElement.querySelector('.btn-text-wechat-timeline');
         var imageWechatTimelineButton = parentElement.querySelector('.btn-image-wechat-timeline');
+        var webPageWechatTimelineButton = parentElement.querySelector('.btn-webpage-wechat-timeline');
+        
         textWechatSessionButton.addEventListener('click',shareTextToWechatSession);
-        imageWechatSessionButton.addEventListener('click',shareImageToWechatSession);
+        imageWechatSessionButton.addEventListener('click',shareImagesToWechatSession);
+        webPageWechatSessionButton.addEventListener('click',shareWebPageToWechatSession);
+        
         textWechatTimelineButton.addEventListener('click',shareTextToWechatTimeline);
-        imageWechatTimelineButton.addEventListener('click',shareImageToWechatTimeline);
+        imageWechatTimelineButton.addEventListener('click',shareImagesToWechatTimeline);
+        webPageWechatTimelineButton.addEventListener('click',shareWebPageToWechatTimeline);
     }
 };
 
 app.initialize();
 
-function shareTextToWechatSession() {
+function shareText(platformType) {
     var text='这是一条测试文本~~~~';
     var shareInfo = {text:text};
-    sharesdk.share(ShareSDK.PlatformType.WechatSession,ShareSDK.ShareType.Text,shareInfo,
+    sharesdk.share(platformType, ShareSDK.ShareType.Text, shareInfo,
                    function(success){},
                    function(fail){});
 }
 
-function shareImageToWechatSession() {
+function shareImages(platformType) {
     var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
     var shareInfo = {images:images};
-    sharesdk.share(ShareSDK.PlatformType.WechatSession,ShareSDK.ShareType.Image,shareInfo,
+    sharesdk.share(platformType, ShareSDK.ShareType.Image, shareInfo,
                    function(success){},
                    function(fail){});
 }
+
+function shareWebPage(platformType) {
+    var icon = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png';
+    var title = '这是网页的标题';
+    var text = '这是网页的内容，android未签名只能分享单张图片到朋友圈';
+    var url = 'http://carhot.cn/articles/1';
+    var shareInfo = {icon:icon, title:title, text:text, url:url};
+    sharesdk.share(platformType, ShareSDK.ShareType.WebPage, shareInfo,
+                   function(success){},
+                   function(fail){});
+}
+
+function shareTextToWechatSession() {
+    shareText(ShareSDK.PlatformType.WechatSession);
+}
+
+function shareImagesToWechatSession() {
+    shareImages(ShareSDK.PlatformType.WechatSession);
+}
+
+function shareWebPageToWechatSession() {
+    shareWebPage(ShareSDK.PlatformType.WechatSession);
+}
+
 
 function shareTextToWechatTimeline() {
-    var text='这是一条测试文本~~~~';
-    var shareInfo = {text:text};
-    sharesdk.share(ShareSDK.PlatformType.WechatTimeline,ShareSDK.ShareType.Text,shareInfo,
-                   function(success){},
-                   function(fail){});
+    shareText(ShareSDK.PlatformType.WechatTimeline);
 }
 
-function shareImageToWechatTimeline() {
-    var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
-    var shareInfo = {images:images};
-    sharesdk.share(ShareSDK.PlatformType.WechatTimeline,ShareSDK.ShareType.Image,shareInfo,
-                   function(success){},
-                   function(fail){});
+function shareImagesToWechatTimeline() {
+    shareImages(ShareSDK.PlatformType.WechatTimeline);
+}
+
+function shareWebPageToWechatTimeline() {
+    shareWebPage(ShareSDK.PlatformType.WechatTimeline);
 }
