8dd31614b036e32eb05dcf2b96e1e65f31415040
behring <zhaolin53636848@126.com>
2017-01-16 10:48:55 +0800

    重构demo分享代码，目前只能分享图片到微信朋友圈和微信好友

--- www/js/index.js
+++ www/js/index.js
@@ -28,230 +28,32 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-        var shareButton = document.getElementById('share-button');
-        shareButton.addEventListener('click',share);
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        var imageWechatSessionButton = parentElement.querySelector('.btn-image-wechat-session');
+        var imageWechatTimelineButton = parentElement.querySelector('.btn-image-wechat-timeline');
+        imageWechatSessionButton.addEventListener('click',shareImageToWechatSession);
+        imageWechatTimelineButton.addEventListener('click',shareImageToWechatTimeline);
     }
 };
 
 app.initialize();
 
-
-var SSDKPlatformType = {
-    /**
-     *  未知
-     */
-    SSDKPlatformTypeUnknown             : 0,
-    /**
-     *  新浪微博
-     */
-    SSDKPlatformTypeSinaWeibo           : 1,
-    /**
-     *  腾讯微博
-     */
-    SSDKPlatformTypeTencentWeibo        : 2,
-    /**
-     *  豆瓣
-     */
-    SSDKPlatformTypeDouBan              : 5,
-    /**
-     *  QQ空间
-     */
-    SSDKPlatformSubTypeQZone            : 6,
-    /**
-     *  人人网
-     */
-    SSDKPlatformTypeRenren              : 7,
-    /**
-     *  开心网
-     */
-    SSDKPlatformTypeKaixin              : 8,
-    /**
-     *  Facebook
-     */
-    SSDKPlatformTypeFacebook            : 10,
-    /**
-     *  Twitter
-     */
-    SSDKPlatformTypeTwitter             : 11,
-    /**
-     *  印象笔记
-     */
-    SSDKPlatformTypeYinXiang            : 12,
-    /**
-     *  Google+
-     */
-    SSDKPlatformTypeGooglePlus          : 14,
-    /**
-     *  Instagram
-     */
-    SSDKPlatformTypeInstagram           : 15,
-    /**
-     *  LinkedIn
-     */
-    SSDKPlatformTypeLinkedIn            : 16,
-    /**
-     *  Tumblr
-     */
-    SSDKPlatformTypeTumblr              : 17,
-    /**
-     *  邮件
-     */
-    SSDKPlatformTypeMail                : 18,
-    /**
-     *  短信
-     */
-    SSDKPlatformTypeSMS                 : 19,
-    /**
-     *  打印
-     */
-    SSDKPlatformTypePrint               : 20,
-    /**
-     *  拷贝
-     */
-    SSDKPlatformTypeCopy                : 21,
-    /**
-     *  微信好友
-     */
-    SSDKPlatformSubTypeWechatSession    : 22,
-    /**
-     *  微信朋友圈
-     */
-    SSDKPlatformSubTypeWechatTimeline   : 23,
-    /**
-     *  QQ好友
-     */
-    SSDKPlatformSubTypeQQFriend         : 24,
-    /**
-     *  Instapaper
-     */
-    SSDKPlatformTypeInstapaper          : 25,
-    /**
-     *  Pocket
-     */
-    SSDKPlatformTypePocket              : 26,
-    /**
-     *  有道云笔记
-     */
-    SSDKPlatformTypeYouDaoNote          : 27,
-    /**
-     *  Pinterest
-     */
-    SSDKPlatformTypePinterest           : 30,
-    /**
-     *  Flickr
-     */
-    SSDKPlatformTypeFlickr              : 34,
-    /**
-     *  Dropbox
-     */
-    SSDKPlatformTypeDropbox             : 35,
-    /**
-     *  VKontakte
-     */
-    SSDKPlatformTypeVKontakte           : 36,
-    /**
-     *  微信收藏
-     */
-    SSDKPlatformSubTypeWechatFav        : 37,
-    /**
-     *  易信好友
-     */
-    SSDKPlatformSubTypeYiXinSession     : 38,
-    /**
-     *  易信朋友圈
-     */
-    SSDKPlatformSubTypeYiXinTimeline    : 39,
-    /**
-     *  易信收藏
-     */
-    SSDKPlatformSubTypeYiXinFav         : 40,
-    /**
-     *  明道
-     */
-    SSDKPlatformTypeMingDao             : 41,
-    /**
-     *  Line
-     */
-    SSDKPlatformTypeLine                : 42,
-    /**
-     *  WhatsApp
-     */
-    SSDKPlatformTypeWhatsApp            : 43,
-    /**
-     *  KaKao Talk
-     */
-    SSDKPlatformSubTypeKakaoTalk        : 44,
-    /**
-     *  KaKao Story
-     */
-    SSDKPlatformSubTypeKakaoStory       : 45,
-    /**
-     *  Facebook Messenger
-     */
-    SSDKPlatformTypeFacebookMessenger   : 46,
-    /**
-     *  支付宝好友
-     */
-    SSDKPlatformTypeAliPaySocial        : 50,
-    /**
-     *  支付宝朋友圈
-     */
-    SSDKPlatformTypeAliPaySocialTimeline: 51,
-    /**
-     *  钉钉
-     */
-    SSDKPlatformTypeDingTalk            : 52,
-    /**
-     *  易信
-     */
-    SSDKPlatformTypeYiXin               : 994,
-    /**
-     *  KaKao
-     */
-    SSDKPlatformTypeKakao               : 995,
-    /**
-     *  印象笔记国际版
-     */
-    SSDKPlatformTypeEvernote            : 996,
-    /**
-     *  微信平台,
-     */
-    SSDKPlatformTypeWechat              : 997,
-    /**
-     *  QQ平台
-     */
-    SSDKPlatformTypeQQ                  : 998,
-    /**
-     *  任意平台
-     */
-    SSDKPlatformTypeAny                 : 999
+function shareImageToWechatTimeline() {
+    var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
+    var shareInfo = {images:images};
+    sharesdk.share(ShareSDK.PlatformType.WechatTimeline,ShareSDK.ShareType.Image,shareInfo,
+                   function(success){},
+                   function(fail){});
 }
 
-function share() {
-    var title = 'hello title';
-    var content = 'this is content!!!';
-    var url = 'http://www.baidu.com';
-    var images = ['http://www.mob.com/images/logo_black.png'];
-    var shareInfo = {platformType:SSDKPlatformType.SSDKPlatformSubTypeWechatSession,title:title,content:content,url:url,images:images};
-    sharesdk.share(function(success){
-                           console.log('----share ok---');
-                           console.log(success);
-                   },
-                   function(fail){
-                           console.log('----share fail---');
-                           console.log(fail);
-                   },
-                   shareInfo);
+function shareImageToWechatSession() {
+    var images = ['http://www.mob.com/images/logo_black.png','https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png'];
+    var shareInfo = {images:images};
+    sharesdk.share(ShareSDK.PlatformType.WechatSession,ShareSDK.ShareType.Image,shareInfo,
+                   function(success){},
+                   function(fail){});
 }
